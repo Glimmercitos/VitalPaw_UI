c59b04c89bd1687cc6c1b184db56132823bbe5d3
violetalisbeth <00007515@uca.edu.sv>
2025-06-09 22:08:45 -0600

    Adding text

--- app/src/main/java/me/vitalpaw/MainActivity.kt
+++ app/src/main/java/me/vitalpaw/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import me.vitalpaw.ui.screens.veterinario.AppointmentScreen
 import me.vitalpaw.ui.screens.veterinario.RegisterScreen
 import me.vitalpaw.ui.theme.VitalPawTheme
 
