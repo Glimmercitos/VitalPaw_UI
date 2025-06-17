40408cf154b72088b1930af139ebf7f9d29196ba
violetalisbeth <00007515@uca.edu.sv>
2025-06-17 00:46:55 -0600

    💄:Adding UI on files and code

--- app/src/main/java/me/vitalpaw/ui/components/icons/TimePickerDialog.kt
+++ app/src/main/java/me/vitalpaw/ui/components/icons/TimePickerDialog.kt
@@ -1,2 +1,31 @@
 package me.vitalpaw.ui.components.icons
 
+import android.app.TimePickerDialog
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import java.time.LocalTime
+
+@Composable
+fun TimePickerDialog(
+    initialTime: LocalTime,
+    onTimeSelected: (LocalTime) -> Unit,
+    onDismiss: () -> Unit
+) {
+    val context = LocalContext.current
+    val hour = initialTime.hour
+    val minute = initialTime.minute
+
+    TimePickerDialog(
+        context,
+        { _, selectedHour, selectedMinute ->
+            val selected = LocalTime.of(selectedHour, selectedMinute)
+            onTimeSelected(selected)
+        },
+        hour,
+        minute,
+        false // false para usar formato 12 horas (AM/PM)
+    ).apply {
+        setOnDismissListener { onDismiss() }
+        show()
+    }
+}
