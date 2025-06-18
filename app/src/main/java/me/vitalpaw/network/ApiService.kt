13502d8c95d5abf6d59407f00286a20bbcc6697f
EmelyChavez <00097923@uca.edu.sv>
2025-06-18 17:33:38 -0600

    ✨: adding ApiService

--- app/src/main/java/me/vitalpaw/network/ApiService.kt
+++ app/src/main/java/me/vitalpaw/network/ApiService.kt
@@ -1,2 +1,29 @@
 package me.vitalpaw.network
 
+import me.vitalpaw.models.User
+import me.vitalpaw.network.request.RegisterRequest
+import me.vitalpaw.network.response.LoginResponse
+import me.vitalpaw.network.response.RegisterResponse
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.POST
+
+interface ApiService {
+    @POST("api/users/register")
+    suspend fun register(
+        @Header("Authorization") token: String,
+        @Body registerRequest: RegisterRequest
+    ): Response<RegisterResponse>
+
+    @POST("api/users/login")
+    suspend fun login(
+        @Header("Authorization") token: String
+    ): Response<LoginResponse>
+
+    @GET("api/users/getUser")
+    suspend fun getUserData(
+        @Header("Authorization") token: String
+    ): Response<User>
+}
